00548b8d829eb2bbc4ae2a79580a621bf309280e
Aleksey_Filkin <acid_wind@mail.ru>
2023-02-16 23:33:03 +0700

    CopyOfArrayTask solved

--- CopyOfArrayTask/Program.cs
+++ CopyOfArrayTask/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿//Написать программу копирования массива
+
+int[] array1 = new int[10];
+int[] array2 = new int[array1.Length];
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++) array[i] = new Random().Next(1, 100);
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++) Console.Write($"{array[i]}  ");
+}
+void CopyArray(int[] array, int[] copyArray)
+{
+    for (int i = 0; i < array.Length; i++) copyArray[i] = array[i];
+}
+FillArray(array1);
+Console.WriteLine("Массив:");
+PrintArray(array1);
+Console.WriteLine();
+CopyArray(array1, array2);
+Console.WriteLine("Скопированный массив:");
+PrintArray(array2);
